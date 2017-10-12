9aba392105851dc2cdd3b36efda84afb69b0636e
kwolfski <kt.schramm.design@gmail.com>
2017-10-11 23:12:20 -0500

    add console log

--- script.js
+++ script.js
@@ -44,8 +44,10 @@ var churchillSpeech = {
 
 //Step 5
   for(i = 0; i < speechesArray.length; i++)
-  if {speechesArray[i].year = oldestSpeech};
-  if {speechesArray[i].year = newestSpeech};
+  if {speechesArray[i].year = oldestSpeech
+    console.log("This is the oldest speech on the page.");};
+  if {speechesArray[i].year = newestSpeech
+    console.log("This is the most recent speech on the page.");};
 
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
